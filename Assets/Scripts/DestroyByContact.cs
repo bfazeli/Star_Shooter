427643de3ccd01cf0df264d2deeaff3de3b8ef8c
bijan102 <bfazeli1@gmail.com>
2018-05-07 16:20:51 -0700

    Update: Instatiated different types of explosions depending on what trigger enters the Asteroid.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    private GameObject theBomb;
+
 	private void OnTriggerEnter(Collider other)
 	{
         if (other.tag == "Boundary")
         {
             return;
         }
+
+        theBomb = Instantiate(explosion, transform.position, transform.rotation) as GameObject;
+
+        if(other.tag == "Player") {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
 	}
