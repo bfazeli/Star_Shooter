1be71e09476210b8a6ab1d782450193533a269b6
bijan102 <bfazeli1@gmail.com>
2018-05-09 11:19:57 -0700

    Update: Added a game over and a restart text to display when the game is over and if the user wished to restart the game.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,36 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
     public GameObject hazard;
     public Vector3 spawnValue;
+    public Text scoreText, restartText, gameOverText;
+
     public int hazardCount;
-    public float startWait, spawnWait;
-    public float waveWait;
-    public Text scoreText;
+    public float startWait, spawnWait, waveWait;
 
     private int score;
+    private bool gameOver, restart;
 
 	private void Start()
 	{
         score = 0;
+        restart = gameOver = false;
+        gameOverText.text = restartText.text = "";
         UpdateScore();
         StartCoroutine(SpawnWaves());
 	}
 
-    /*
+	private void Update()
+	{
+        // If player chooses to restart the game, check to confirm
+        // that the user did indeed press the R key
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+	/*
      * SpawnWaves:  
      *      Creates as many tumbling Asteroids based on the hazardCount
      *      from a random horizontal pt that enters the scene 
      *      outside of the game area.
      *
      */
-    IEnumerator SpawnWaves() {
+	IEnumerator SpawnWaves() {
         
         yield return new WaitForSeconds(startWait);
-        while (true)
+        while (!gameOver)
         {
             for (int i = 0; i < hazardCount; ++i)
             {
@@ -42,6 +59,10 @@ public class GameController : MonoBehaviour {
             }
             yield return new WaitForSeconds(waveWait);
         }
+
+        // After the game is over, give the player an option to restart.
+        restartText.text = "Press 'R' for Restart";
+        restart = true;
     }
 
     void UpdateScore() {
@@ -51,5 +72,10 @@ public class GameController : MonoBehaviour {
     public void AddScore(int newScoreValue) {
         score += newScoreValue;
         UpdateScore();
+    }
+
+    public void GameOver() {
+        gameOverText.text = "Game Over";
+        gameOver = true;
     }
 }
