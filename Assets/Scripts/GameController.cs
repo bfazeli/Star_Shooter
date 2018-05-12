93749471655e64a49c6054d2cbfa72a6830ea779
bijan102 <bfazeli1@gmail.com>
2018-05-12 13:50:57 -0700

    Update: Added a few asteroid prefabs and repeated the background.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValue;
     public Text scoreText, restartText, gameOverText;
 
@@ -52,6 +52,7 @@ public class GameController : MonoBehaviour {
         {
             for (int i = 0; i < hazardCount; ++i)
             {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
